d6c7d74be59a84f5fb3dde8a619ad39b0fa41ac5
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-08-05 15:05:59 +0200

    use Requests to fetch documents from api.anyfetch.com

--- anyfetch.py
+++ anyfetch.py
@@ -4,7 +4,7 @@
 import sys
 
 from workflow import Workflow, web
-from requests import Requests
+import requests
 
 
 def main(wf):
@@ -19,13 +19,13 @@ def main(wf):
 
     # Do stuff here ...
 
-    json = web.get('https://localhost:3000/app/documents?data=%7B%22sessionId%22%3A%22fake_session_id%22%2C%22salesFetchURL%22%3A%22https%3A%2F%2Flocalhost%3A3000%22%2C%22instanceURL%22%3A%22https%3A%2F%2Feu0.salesforce.com%22%2C%22context%22%3A%7B%22templatedDisplay%22%3A%22Matthieu%20Bacconnier%22%2C%22templatedQuery%22%3A%22Matthieu%20Bacconnier%22%2C%22recordId%22%3A%220032000001DoV22AAF%22%2C%22recordType%22%3A%22Contact%22%7D%2C%22user%22%3A%7B%22id%22%3A%2200520000003RnlGAAS%22%2C%22name%22%3A%22mehdi%40anyfetch.com%22%2C%22email%22%3A%22tanguy.helesbeux%40insa-lyon.fr%22%7D%2C%22organization%22%3A%7B%22id%22%3A%2200D20000000lJVPEA2%22%2C%22name%22%3A%22AnyFetch%22%7D%2C%22hash%22%3A%22ZS5ZybJaJPiPAvPms9DNw4Nd2y0%3D%22%7D').json()
+    r = requests.get('https://api.anyfetch.com/documents', auth=('tanguyhelesbeux@gmail.com', 'bitecouille'))
+    documents = r.json()['data']
 
-    documents = json['documents']['data']
-
-    # Add an item to Alfred feedback
+    # Add items to Alfred feedback
     for document in documents:
         wf.add_item(document['id'], u'Item subtitle')
+
 
     # Send output to Alfred
     wf.send_feedback()
